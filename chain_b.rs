5377b380f516721fb35f9160a49bda57820b29ff
hellcatxrp <james.baker26@hotmail.co.uk>
2024-12-22 13:14:29 +1300

    update1

--- chain_b.rs
+++ chain_b.rs
@@ -10,7 +10,6 @@ pub struct block {
 }
 pub async fn run_chain_b(mut rx: Receiver<Block>) {
     println!("Chain B (Proof of Authority) is running...");
-    while let Some(block) = rx.recv().await {
-        println!(Chain B received block: {:?}", block);
-    }
-}
+    while let Some(block) = rx.recv().await }
+        println!("Chain B received block: {:?}", block);
+    
